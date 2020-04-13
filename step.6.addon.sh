582e262bafc56f1fcccbbec4ef0a212fe46c3b8d
root <root@bastion.c.redislabs-sa-training-services.internal>
2020-04-13 06:42:28 +0000

    loki

--- step.6.addon.sh
+++ step.6.addon.sh
@@ -1,3 +1,5 @@
 #ansible-playbook -i inventory.ini  addon.yaml
-#ansible-playbook -i inventory.ini   addon.step.1.storage.yaml
-ansible-playbook -i inventory.ini   addon.step.2.cassandra.yaml
+#ansible-playbook -i inventory.ini  addon.step.1.storage.yaml
+#ansible-playbook -i inventory.ini  addon.step.2.cassandra.yaml
+#ansible-playbook -i inventory.ini  addon.step.3.cortex.yaml 
+ansible-playbook -i inventory.ini  addon.step.4.loki.yaml
