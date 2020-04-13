32299e057a6a6861e52ebe885394023ca1811ee9
root <root@bastion.c.redislabs-sa-training-services.internal>
2020-04-13 01:45:26 +0000

    cassandra

--- step.6.addon.sh
+++ step.6.addon.sh
@@ -1,2 +1,3 @@
 #ansible-playbook -i inventory.ini  addon.yaml
-ansible-playbook -i inventory.ini   addon.step.1.storage.yaml
+#ansible-playbook -i inventory.ini   addon.step.1.storage.yaml
+ansible-playbook -i inventory.ini   addon.step.2.cassandra.yaml
